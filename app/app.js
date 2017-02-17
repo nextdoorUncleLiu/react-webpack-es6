5b796283099e755bee962d75065511fa6b24926c
lxc <a260496725@qq.com>
2017-02-17 18:10:45 +0800

    1、新增多个子组件加载写法 2、新增setState方法调用 3、新增JSX写法 4、新增事件调用

--- app/app.js
+++ app/app.js
@@ -1,5 +1,50 @@
 import React from 'react';
 import {Component} from './component/component.js';
+import {Components} from './component/component2.js';
 import {render} from 'react-dom';
 
-render(<Component />,document.querySelector('#aaa'));
+export class ParseNode extends React.Component {
+	constructor(){
+		super();
+		this.name = '张三';
+		this.img = <img src="../build/img/1.png" style={{width:'100px',height:'100px'}} />;
+		this.state = {
+			count:0
+		}
+		this.handClick = this.handClick.bind(this);
+	}
+	handClick() {
+		++this.state.count;
+		let count = this.state.count;
+		switch(count){
+			case 1:{
+				this.name='李四';
+			}
+				break;
+			case 2:{
+				this.name='王五';
+			}
+				break;
+			case 3:{
+				this.name='赵六';
+			}
+				break;
+		}
+		this.setState({
+			count
+		});
+	}
+	render() {
+		return (
+			<div>
+				<button onClick={this.handClick}>{this.state.count}</button>
+				<Component name={this.name}>
+					{this.img};
+				</Component>
+				<Components name={this.name} />
+			</div>
+		)
+	}
+}
+
+render( < ParseNode / > , document.querySelector('#aaa'));
--- app/component/component2.js
+++ app/component/component2.js
@@ -1,11 +1,13 @@
 import React from 'react';
 let path = require('path');
 
-export class Component extends React.Component {
+
+export class Components extends React.Component {
+
 	render() {
 		return (
 			<div style={{width: '100px',background: '#000',overflow:'hidden',color:'#fff',float:'left'}}>
-				{this.props.children}
+				<img src='../build/img/1.png' style={{width:'100px',height:'100px'}} />
 				{this.props.name}
 			</div>
 		)
